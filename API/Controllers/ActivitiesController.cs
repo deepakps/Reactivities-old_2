8718946510fde1c4d5158d4bd2abd018da8d037d
Deepak Shinde <deepak27shinde@gmail.com>
2023-02-13 23:52:43 +0530

    Section 3:35 - Adding Details Handler.

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -9,23 +9,16 @@ namespace API.Controllers
 {
     public class ActivitiesController : BaseApiController
     {
-        private readonly IMediator _mediator;
-        public ActivitiesController(IMediator mediator)
-        {
-            _mediator = mediator;
-        }
-
         [HttpGet] //api/activities
         public async Task<ActionResult<List<Activity>>> GetActivities()
         {
-            return await _mediator.Send(new List.Query());
+            return await Mediator.Send(new List.Query());
         }
 
         [HttpGet(("{id}"))] //api/activities/{id}
         public async Task<ActionResult<Activity>> GetActivity(Guid id)
         {
-            //return await _context.Activities.FindAsync(id);
-            return Ok();
+            return await Mediator.Send(new Details.Query { Id = id });
         }
     }
 }
