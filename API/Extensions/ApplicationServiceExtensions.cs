de58d454cc700346f2a99844df3ab273e769a542
Deepak Shinde <deepak27shinde@gmail.com>
2023-02-18 15:37:39 +0530

    Section 4:40 - Startup class housekeeping.

---
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,38 @@
+using Application.Activities;
+using Application.Core;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        // Code commented in Program.cs & moved in AddApplicationServices. This simplifies & makes Program.cs more cleaner.
+        // Date - 18th Feb, 2023.
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+        {
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+            services.AddDbContext<DataContext>(opt =>
+            {
+                opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
+            });
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                });
+            });
+
+            services.AddMediatR(typeof(List.Handler));
+            // Below statements checks the Assembly and register all the mapping profiles. Similar to above statement.
+            // Date - 14th Feb, 2023.
+            services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+
+            return services;
+        }
+    }
+}
