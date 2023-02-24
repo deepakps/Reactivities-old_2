145635baa476d91191bd1817368f7c75bb8fe312
Deepak Shinde <deepak27shinde@gmail.com>
2023-02-24 23:31:57 +0530

    Section 5:50 - Creating an activity list.

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Grid, List } from "semantic-ui-react";
 import { Activity } from "../../../app/models/activity";
+import ActivityList from "./ActivityList";
 
 interface Props {
     activities: Activity[];
@@ -10,13 +11,9 @@ export default function ActivityDashboard({ activities }: Props) {
     return (
         <Grid>
             <Grid.Column width='10'>
-                <List>
-                    {activities.map(activity => (
-                        <List.Item key={activity.id}>
-                            {activity.title}
-                        </List.Item>
-                    ))}
-                </List>
+                {/* List & List.Item code shifted to ActivityList.tsx.
+                    Date - 22nd Feb, 2023. */}
+                <ActivityList activities={activities} />
             </Grid.Column>
         </Grid>
     )
