9867af7cdf60201d52cba70f32a533d3edaf0d7b
Deepak Shinde <deepak27shinde@gmail.com>
2023-02-24 21:40:56 +0530

    Section 5:48 - Adding a Nav Bar.

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -9,6 +9,7 @@ import axios from 'axios';
 import { Header } from 'semantic-ui-react';
 import List from 'semantic-ui-react/dist/commonjs/elements/List';
 import { Activity } from '../models/activity';
+import NavBar from './NavBar';
 // Commented demo code.
 // Date - 11th Feb, 2023.
 // import { ducks } from './demo';
@@ -30,12 +31,18 @@ function App() {
 
   return (
     <div>
-      <Header as='h2' icon='users' content='Reactivities' />
+      {/* Commented as part of Semantic UI changes.
+      Date - 22nd Feb, 2023. */}
+      {/* <Header as='h2' icon='users' content='Reactivities' /> */}
+
+      {/* Demo code. */}
       {/* {ducks.map(duck => (
           // When we are looping over in react then you will need to assign key for opening element.
           // Date - 11th Feb, 2023. 
           <DuckItem duck={duck} key={duck.name} />
         ))} */}
+
+      <NavBar />
       <List>
         {activities.map(activity => (
           <List.Item key={activity.id}>
