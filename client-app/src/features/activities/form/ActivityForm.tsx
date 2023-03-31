45efea9617350003baad6079a27667573090ec17
Deepak Shinde <deepak27shinde@gmail.com>
2023-03-31 17:59:20 +0530

    Section 5:55 - Edt activity & form basics in react

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { ChangeEvent, useState } from "react";
 import { Button, Form, Segment } from "semantic-ui-react";
 import { Activity } from "../../../app/models/activity";
 
@@ -9,18 +9,43 @@ interface Props {
     activity: Activity | undefined;
 }
 
-export default function ActivityForm({ closeForm, activity }: Props) {
+export default function ActivityForm({ closeForm, activity: selectedActivity }: Props) {
+
+    const initialState = selectedActivity ?? {
+        id: '',
+        title: '',
+        category: '',
+        description: '',
+        date: '',
+        city: '',
+        venue: ''
+    }
+
+    const [activity, setActivity] = useState(initialState);
+
+    function handleSubmit() {
+        console.log(activity);
+    }
+
+    // After simply declaring value property we are broking typing into the input field. That is the reason react makes the field readonly 
+    // as react unable to track changes. That is the reason we handle onChange inside the input field.
+    // Date - 06th Mar, 2023.
+    function handleInputChange(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
+        const { name, value } = event.target;
+        setActivity({ ...activity, [name]: value });
+    }
+
     return (
         // clearing property to clear any previous floats in HTML.
         // Date - 26th Feb, 2023.
         <Segment clearing>
-            <Form>
-                <Form.Input placeholder='Title' />
-                <Form.TextArea placeholder='Description' />
-                <Form.Input placeholder='Category' />
-                <Form.Input placeholder='Date' />
-                <Form.Input placeholder='City' />
-                <Form.Input placeholder='Venue' />
+            <Form onSubmit={handleSubmit} autoComplete='off'>
+                <Form.Input placeholder='Title' value={activity.title} name='title' onChange={handleInputChange} />
+                <Form.TextArea placeholder='Description' value={activity.description} name='description' onChange={handleInputChange} />
+                <Form.Input placeholder='Category' value={activity.category} name='category' onChange={handleInputChange} />
+                <Form.Input placeholder='Date' value={activity.date} name='date' onChange={handleInputChange} />
+                <Form.Input placeholder='City' value={activity.city} name='city' onChange={handleInputChange} />
+                <Form.Input placeholder='Venue' value={activity.venue} name='venue' onChange={handleInputChange} />
                 <Button floated="right" positive type="submit" content="Submit" />
                 <Button onClick={closeForm} floated="right" type="button" content="Cancel" />
             </Form>
