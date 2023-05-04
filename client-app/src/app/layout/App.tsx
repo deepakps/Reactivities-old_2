0a1ee28494af4e608d909c7a69faf0fd7c64657d
Deepak Shinde <deepak27shinde@gmail.com>
2023-05-04 12:26:42 +0530

    Section 8:80 - Adding routes.

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,23 +1,13 @@
 // This is App which gets rendered inside src>index.tsx.
 // Date - 10th Feb, 2023.
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Container } from 'semantic-ui-react';
 import NavBar from './NavBar';
-import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
-import LoadingComponent from './LoadingComponent';
-import { useStore } from '../stores/store';
 import { observer } from 'mobx-react-lite';
+import { Outlet } from 'react-router-dom';
 
 function App() {
-  const { activityStore } = useStore();
-
-  useEffect(() => {
-    activityStore.loadActivities();
-  }, [activityStore]);
-
   /* Code refactored & shifted to activityStore.ts. Date - 26th Apr, 2023.*/
-
-  if (activityStore.loadingInitial) return <LoadingComponent content='Loading app...' />
 
   return (
     // 'Div' is replaced with 'Fragment'. Another reason is that we are not allowed to put two separate elements of same level inside react component.
@@ -28,9 +18,10 @@ function App() {
       <NavBar />
 
       <Container style={{ marginTop: "7em" }}>
-        {/* List & List.Item code shifted to ActivityDashboard.tsx.
-            Date - 22nd Feb, 2023. */}
-        <ActivityDashboard />
+        {/* Commented ActivityDashboard as Implemented Routing.
+        Here, Outlet is swapped with requested page. 
+        Date - 04th May, 2023. */}
+        <Outlet />
       </Container>
       {/* </div> */}
     </>
--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Grid } from "semantic-ui-react";
 import ActivityDetails from "../Details/ActivityDetails";
 import ActivityForm from "../form/ActivityForm";
 import ActivityList from "./ActivityList";
 import { useStore } from "../../../app/stores/store";
 import { observer } from "mobx-react-lite";
+import LoadingComponent from "../../../app/layout/LoadingComponent";
 
 // Date - 21st Feb, 2023.
 export default observer(function ActivityDashboard() {
     const { activityStore } = useStore();
     const { selectedActivity, editMode } = activityStore;
+
+    useEffect(() => {
+        activityStore.loadActivities();
+    }, [activityStore]);
+
+    if (activityStore.loadingInitial) return <LoadingComponent content='Loading app...' />
 
     return (
         <Grid>
