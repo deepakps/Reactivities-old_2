aa5e05873d84be06c9f73d0e181a9f52993d455c
Deepak Shinde <deepak27shinde@gmail.com>
2023-05-09 19:13:52 +0530

    Section 8:89 - Cleaning up unused code.

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -8,10 +8,11 @@ import LoadingComponent from "../../../app/layout/LoadingComponent";
 // Date - 21st Feb, 2023.
 export default observer(function ActivityDashboard() {
     const { activityStore } = useStore();
+    const { loadActivities, activityRegistry } = activityStore;
 
     useEffect(() => {
-        activityStore.loadActivities();
-    }, [activityStore]);
+        if (activityRegistry.size <= 1) loadActivities();
+    }, [loadActivities]);
 
     if (activityStore.loadingInitial) return <LoadingComponent content='Loading app...' />
 
