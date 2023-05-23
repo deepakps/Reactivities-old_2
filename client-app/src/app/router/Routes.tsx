ea7aa870b50623ba6b7a8a2ffac12d2edbdfce52
Deepak Shinde <deepak27shinde@gmail.com>
2023-05-23 20:30:26 +0530

    Section 9:96 - Activity details page.

--- client-app/src/app/router/Routes.tsx
+++ client-app/src/app/router/Routes.tsx
@@ -2,7 +2,7 @@ import { RouteObject, createBrowserRouter } from "react-router-dom";
 import App from "../layout/App";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 import ActivityForm from "../../features/activities/form/ActivityForm";
-import ActivityDetails from "../../features/activities/Details/ActivityDetails";
+import ActivityDetails from "../../features/activities/details/ActivityDetails";
 
 export const routes: RouteObject[] = [
     {
